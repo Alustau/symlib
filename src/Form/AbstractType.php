7c6104c71d53ea85da4c39ae16c315208bc6151c
alustau <denisalustau@gmail.com>
2016-03-01 09:45:38 -0300

    First commit

---
+++ src/Form/AbstractType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Form;
+
+use Symfony\Component\Form\AbstractType as Type;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+
+abstract class AbstractType extends Type
+{
+    protected $className;
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+//         $em = $this->getDoctrine()->getManager();
+//         $em = $this->getDoctrine()->getManager();
+////        $columns = $em->getClassMetadata('AppBundle\Entity\Equipe')->fieldMappings;
+//        $columns = $em->getClassMetadata('AppBundle\Entity\Equipe');
+        
+//        $builder
+//            ->add('nome', 'text', ['required' => false]);
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => $this->className]);
+    }
+}
