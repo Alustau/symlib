0e65e7872e0c6116b2af7eae0a5f8292b65f0cab
alustau <denisalustau@gmail.com>
2016-03-03 14:52:29 -0300

    Adding the author's projects and dependency-injection package

--- src/Form/AbstractType.php
+++ src/Form/AbstractType.php
@@ -5,11 +5,12 @@ namespace Form;
 use Symfony\Component\Form\AbstractType as Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\DependencyInjection\ContainerAware;
-
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
 abstract class AbstractType extends Type
 {
+    use ContainerAwareTrait;
+    
     protected $className;
     
     public function buildForm(FormBuilderInterface $builder, array $options)
